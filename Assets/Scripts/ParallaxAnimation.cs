3cb862393b1635bbf14887e6c01ce7de2cba245b
Angel Vil <vilaplanaangel@gmail.com>
2020-11-28 20:50:53 +0100

    [MOD] Solve bugs

---
+++ Assets/Scripts/ParallaxAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxAnimation : MonoBehaviour
+{
+    public RawImage image;
+    public float parallaxSpeed = 0.02f;
+
+    public void Update()
+    {
+        float finalspeed = parallaxSpeed * Time.deltaTime;
+        image.uvRect = new Rect(0, image.uvRect.y + finalspeed, 1f, 1f);
+    }
+}
